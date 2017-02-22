c279f9011342f2dcaa782b01af4242828d709220
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-21 20:57:23 -0500

    implemented turn base questionaire successfully

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
 class Game < ApplicationRecord
   belongs_to :user
+
+  validates :playerOneWon, presence: true
+  validates :playerTwoWon, presence: true
+  validates :over, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
 class Question < ApplicationRecord
+  validates :problem, presence: true
+  validates :correct, presence: true
+  validates :wrongOne, presence: true
+  validates :wrongTwo, presence: true
+  validates :wrongThree, presence: true
 end
