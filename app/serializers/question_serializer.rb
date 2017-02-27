a940e714ab8bb749d73aa9d74284aea75f486b42
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-27 15:35:19 -0500

    Add attribute user_id to question serializer

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :problem, :correct, :wrongOne, :wrongTwo, :wrongThree
+  attributes :id, :problem, :correct, :wrongOne, :wrongTwo, :wrongThree, :user_id
 end
