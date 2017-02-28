39aa0b99b070ba54b982258bc9c8d9cf78242084
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-28 11:36:45 -0500

    Took out examples

--- db/examples.rb
+++ db/examples.rb
@@ -1,107 +1,88 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to experiment with
-# your app during development.
+
+# Question.create!(
+#   problem: 'What would you have if you had five mango and two bananas in one hand and two mango and four bananas in the other hand?',
+#   correct: 'Very large hands',
+#   wrongOne: 'mangos and bananas',
+#   wrongTwo: 'handful of fruits',
+#   wrongThree: 'Nothing'
+# )
 #
-# The data can then be loaded with the rake db:examples (or created alongside
-# the db with db:nuke_pave).
+# Question.create!(
+#   problem: 'What is the capital of Panama?',
+#   correct: 'Panama City',
+#   wrongOne: 'El Panama',
+#   wrongTwo: 'Pajama',
+#   wrongThree: 'Banana'
+# )
 #
-# Examples:
+# Question.create!(
+#   problem: 'What is the third law of Motion?',
+#   correct: 'For every action, there is an equal and opposite reaction',
+#   wrongOne: 'Relationship between force, mass and acceleration',
+#   wrongTwo: 'Law of Inertia',
+#   wrongThree: 'Gravity'
+# )
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# %w(antony jeff matt jason).each do |name|
-#   email = "#{name}@#{name}.com"
-#   next if User.exists? email: email
-#   User.create!(email: email,
-#                password: 'abc123',
-#                password_confirmation: nil)
-# end
-
-Question.create!(
-  problem: 'What would you have if you had five mango and two bananas in one hand and two mango and four bananas in the other hand?',
-  correct: 'Very large hands',
-  wrongOne: 'mangos and bananas',
-  wrongTwo: 'handful of fruits',
-  wrongThree: 'Nothing'
-)
-
-Question.create!(
-  problem: 'What is the capital of Panama?',
-  correct: 'Panama City',
-  wrongOne: 'El Panama',
-  wrongTwo: 'Pajama',
-  wrongThree: 'Banana'
-)
-
-Question.create!(
-  problem: 'What is the third law of Motion?',
-  correct: 'For every action, there is an equal and opposite reaction',
-  wrongOne: 'Relationship between force, mass and acceleration',
-  wrongTwo: 'Law of Inertia',
-  wrongThree: 'Gravity'
-)
-
-Question.create!(
-  problem: 'What is the first law of Motion?',
-  correct: 'Law of Inertia',
-  wrongOne: 'Relationship between force, mass and acceleration',
-  wrongTwo: 'For every action, there is an equal and opposite reaction',
-  wrongThree: 'Gravity'
-)
-
-Question.create!(
-  problem: 'What is the second law of Motion?',
-  correct: 'Relationship between force, mass and acceleration',
-  wrongOne: 'For every action, there is an equal and opposite reaction',
-  wrongTwo: 'Law of Inertia',
-  wrongThree: 'Gravity'
-)
-
-Question.create!(
-  problem: 'Where do Pineapple grow on?',
-  correct: 'Bush',
-  wrongOne: 'Tree',
-  wrongTwo: 'Ground',
-  wrongThree: 'PPAP'
-)
-
-Question.create!(
-  problem: 'Why did the Mathematician install a knockers on his door?',
-  correct: 'For the Nobel Prize',
-  wrongOne: 'So people can knock on it',
-  wrongTwo: 'It looks good on the door',
-  wrongThree: 'He likes it'
-)
-
-Question.create!(
-  problem: 'How, What, and Where?',
-  correct: 'Modeling',
-  wrongOne: 'Huh?',
-  wrongTwo: 'Right...',
-  wrongThree: 'Need more Info'
-)
-
-Question.create!(
-  problem: 'How did the word, debug, came to be?',
-  correct: 'Grace Hopper',
-  wrongOne: 'Alexander Russell',
-  wrongTwo: 'It was always there',
-  wrongThree: 'Dennis Ritchie'
-)
-
-Question.create!(
-  problem: 'The language C was developed by...',
-  correct: 'Both of them',
-  wrongOne: 'Neither of them',
-  wrongTwo: 'Dennis Ritchie',
-  wrongThree: 'Ken Thompson'
-)
-
-Question.create!(
-  problem: 'Who invented the first computer resembling todays modern machines',
-  correct: 'Charles Babbage',
-  wrongOne: 'William Oughtred',
-  wrongTwo: 'Dennis Ritchie',
-  wrongThree: 'Grace Hopper'
-)
+# Question.create!(
+#   problem: 'What is the first law of Motion?',
+#   correct: 'Law of Inertia',
+#   wrongOne: 'Relationship between force, mass and acceleration',
+#   wrongTwo: 'For every action, there is an equal and opposite reaction',
+#   wrongThree: 'Gravity'
+# )
+#
+# Question.create!(
+#   problem: 'What is the second law of Motion?',
+#   correct: 'Relationship between force, mass and acceleration',
+#   wrongOne: 'For every action, there is an equal and opposite reaction',
+#   wrongTwo: 'Law of Inertia',
+#   wrongThree: 'Gravity'
+# )
+#
+# Question.create!(
+#   problem: 'Where do Pineapple grow on?',
+#   correct: 'Bush',
+#   wrongOne: 'Tree',
+#   wrongTwo: 'Ground',
+#   wrongThree: 'PPAP'
+# )
+#
+# Question.create!(
+#   problem: 'Why did the Mathematician install a knockers on his door?',
+#   correct: 'For the Nobel Prize',
+#   wrongOne: 'So people can knock on it',
+#   wrongTwo: 'It looks good on the door',
+#   wrongThree: 'He likes it'
+# )
+#
+# Question.create!(
+#   problem: 'How, What, and Where?',
+#   correct: 'Modeling',
+#   wrongOne: 'Huh?',
+#   wrongTwo: 'Right...',
+#   wrongThree: 'Need more Info'
+# )
+#
+# Question.create!(
+#   problem: 'How did the word, debug, came to be?',
+#   correct: 'Grace Hopper',
+#   wrongOne: 'Alexander Russell',
+#   wrongTwo: 'It was always there',
+#   wrongThree: 'Dennis Ritchie'
+# )
+#
+# Question.create!(
+#   problem: 'The language C was developed by...',
+#   correct: 'Both of them',
+#   wrongOne: 'Neither of them',
+#   wrongTwo: 'Dennis Ritchie',
+#   wrongThree: 'Ken Thompson'
+# )
+#
+# Question.create!(
+#   problem: 'Who invented the first computer resembling todays modern machines',
+#   correct: 'Charles Babbage',
+#   wrongOne: 'William Oughtred',
+#   wrongTwo: 'Dennis Ritchie',
+#   wrongThree: 'Grace Hopper'
+# )
