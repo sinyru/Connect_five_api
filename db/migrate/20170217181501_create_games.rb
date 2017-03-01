9786c626dfa63759b9dfcb9c67f4280d7d2908ac
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-03-01 17:03:32 -0500

    Change Schema
    -changed the schema of game table

--- db/migrate/20170217181501_create_games.rb
+++ db/migrate/20170217181501_create_games.rb
@@ -2,9 +2,9 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.string :playerOneWon, default: false, null: false
-      t.string :playerTwoWon, default: false, null: false
-      t.string :over, default: false, null: false
+      t.boolean :playerOneWon, default: false, null: false
+      t.boolean :playerTwoWon, default: false, null: false
+      t.boolean :over, default: false, null: false
 
       t.timestamps
     end
